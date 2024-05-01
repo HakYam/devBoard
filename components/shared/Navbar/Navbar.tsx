557a4c6ae9738dd09daeacbc8af1ec455a199daf
Hakan <hakan@yaman.uk>
2024-05-01 22:20:34 +0300

    ui lib

--- components/shared/Navbar/Navbar.tsx
+++ components/shared/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href={"/"} className="flex items-center gap-1">
         <Image
           src={"/assets/images/site-logo.svg"}
@@ -33,9 +33,10 @@ const Navbar = () => {
             }
         }} />
         </SignedIn>
+        mob navbar
       </div>
-    </div>
-  );
-};
+    </nav>
+  )
+}
 
 export default Navbar;
